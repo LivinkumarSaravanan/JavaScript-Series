fc33bea9a7540d8f01363705bc43685319c733b4
LivinkumarSaravanan <124226007+LivinkumarSaravanan@users.noreply.github.com>
2026-03-29 09:03:48 +0530

    Add files via upload

---
+++ 3.Console&Comments/script.js
@@ -0,0 +1,9 @@
+// these lines are used to print out data in console
+console.log(10);
+console.log(11);
+console.log(12);
+console.log(13);
+console.log(14);
+
+console.log("Leo");
+console.log("Leo");
