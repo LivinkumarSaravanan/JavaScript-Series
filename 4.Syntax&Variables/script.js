04f2a1fc0ee0e4ffa52ebb4425a86a7d89aab2f2
LeoScript <124226007+LivinkumarSaravanan@users.noreply.github.com>
2026-04-06 20:51:07 +0530

    Add files via upload

---
+++ 4.Syntax&Variables/script.js
@@ -0,0 +1,73 @@
+// console.log(10);console.log(12)
+
+
+// console.log("Hi");
+// console.log("Hi");
+
+// let a="Michael Madana Kama Rajan";
+
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+// console.log(a);
+
+// let b = 12;
+// b=15
+// console.log(b);
+
+
+
+
+// var a = 10;
+// a=12;
+// var a = "Leo"
+// console.log(a);
+
+
+
+
+
+// let a = 10;
+// a="Hi"
+// var a=80;
+// console.log(a);
+
+
+
+// const pi = 3.14;
+// pi=4;
+// let pi = 4;
+// console.log(pi);
+
+
+
+// let name = "Leo"
+// var fname = "Das"
+// console.log(name);
+
+
+
+let Hello=12
+
+
+// let 1a=12
+
+// let first_name = "Leo"
+
+let name="Alex";
+// console.log(Name);
+
+// let let=12;
+
+let latest_movie="God Father"
+console.log(latest_movie);
+
+
+
+
+
+
+
+
+
---
+++ 5.Datatypes/script.js
@@ -0,0 +1,61 @@
+//Number
+// let a = 10;
+// let b= 9.99;
+// console.log(typeof a);
+// console.log(a+b);
+
+// String
+let myName = 'Leo';
+let myJob = "FED"
+// console.log(typeof myName);
+
+// console.log(myName+myJob); //LeoFED
+
+// Boolean 
+
+// let a=true;
+// let b = false;
+// console.log(typeof b);
+
+//Undefined
+
+// let a;
+// console.log(typeof a);
+
+// Null
+// let a = null;
+// console.log(a);
+
+// BigInt
+// let a = 987249587329465893745689879879876896n;
+// console.log(typeof a);
+
+
+// let a = 10;
+// let b = a;
+
+// a=20;
+// console.log(b);
+
+// let a = object 
+
+// let a = 10;
+// let b = a;
+
+// a+=10
+
+// console.log(a);
+// console.log(b);
+
+
+let a ={
+    name: "hen",
+    age:14
+}
+
+let b = a;
+
+a.age=41
+
+console.log(a);
+console.log(b);
