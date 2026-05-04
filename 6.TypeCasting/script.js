2627f5a1c94716fda97178712dd9623997709ed0
LeoScript <124226007+LivinkumarSaravanan@users.noreply.github.com>
2026-05-04 17:40:44 +0530

    Add files via upload

---
+++ 6.TypeCasting/script.js
@@ -0,0 +1,48 @@
+// let a = Number("200"); //"200" --> 200
+// let a = String(100)
+// let a = Boolean("ujhgfui")
+// console.log(a);
+// console.log(typeof a);
+
+// let a = String(100);
+
+
+// console.log("5"+2); //2 --> "2" --> "52"
+// console.log("5"-2); //"5" -->5 
+// console.log("7"+2);
+
+// console.log(false+1);
+
+// console.log(Number("5")+6);
+
+// let a = 5
+// let b = "3"
+
+// console.log(a+b);
+
+
+// let a = prompt("Enter your name: ")
+// console.log(a);
+// console.log(a);
+// console.log(a);
+
+
+// let a =prompt() //100 --> "100", "hi"
+// console.log(a);
+// console.log(a);
+// console.log(a);
+
+let num = prompt("Enter your number: ") //"600"
+console.log(Number(num)+100); //600+100
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 7.Operators/script.js
@@ -0,0 +1,65 @@
+// let a = 10
+// let b = 12
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+
+// Assignment Operators
+// let a= 10
+// a+=3 //13
+// a+=10 //23
+// a-=5 //18
+// a*=7 //126
+// a/=5
+// console.log(a);
+
+
+// Comparison Operators
+// console.log(10<-10);
+// console.log(10>9);
+
+// Logocal Operators
+
+// console.log(false && false);
+// console.log(false || false);
+
+//&& --> false --> false
+//|| --> true  --> true
+
+// console.log(true && true);
+// console.log(true || false);
+
+// console.log(!false);
+
+// let a = 10;
+// console.log(a<10 && a>5);
+
+
+// Unary Operators
+
+// let a = 10; //11
+// a++
+// ++a
+
+// let b = a++;
+// let b = ++a; //11
+
+// --a
+// a--
+
+// console.log("b: ",b);
+// console.log("a: ",a); //11
+
+// console.log(++a + a++);
+// console.log(--a - a-- - a - --a);
+
+// Ternary Operators 
+
+// let age = 1;
+
+// let canVote = age>=18 ? "yes" : "no"
+
+// console.log(canVote);
